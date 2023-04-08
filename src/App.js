114db314a4d2ee175fd0f6c91e116a452b111986
zkjiang12 <zkjiang12@gmail.com>
2023-04-08 15:41:38 -0400

    added anonymity choice,userinfo page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import {React,useState} from 'react'
 import Navbar from './components/Navbar';
 import Form from './components/Form';
 import Display from './components/Display';
 
 function App() {
+  const [login,setLogin] = useState(false) // if logged in or not
+  const [anonymity,setAnonymity] = useState(false) //anonymous or not
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar 
+        login = {login} 
+        setLogin = {setLogin}
+        anonymity = {anonymity}
+        setAnonymity = {setAnonymity}
+      />
       <Form/>
       <Display/>
     </div>
